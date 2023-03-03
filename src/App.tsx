b9c88894003b6602e475251d1646c5105935ec50
Nick Loza <nicklozadeveloper@gmail.com>
2023-03-03 17:47:21 -0300

    add: loading page spinner added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import './sass/index.scss'
 
- import Layout from './components/layout/Layout';
+import Layout from './components/layout/Layout';
+import { useEffect, useState } from 'react';
+import ClipLoader from "react-spinners/ClipLoader";
+import DotLoader from 'react-spinners/DotLoader';
 
 function App() {
+  const [ loading, setLoading ] = useState<boolean>(false)
 
-  return <Layout/>
+  useEffect(()=>{
+    setLoading(true);
+    setTimeout(()=>{
+      setLoading(false)
+    },2000)
+  },[])
+
+  return (
+    <div className='app'>{loading ? 
+        <DotLoader
+        className='app__spinner'
+        color={"#99CCFF"}
+        loading={loading}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+    :
+      <Layout/>}
+    </div>
+  ) 
 }
 
 export default App
